7774b03f2bd88264553dcd5607505c7327d90cf8
theunstopabble <gautamkumar43421@gmail.com>
2025-12-27 02:01:12 +0530

    Fix: Updated BASE_URL for Android and set app name to SwadKart

--- frontend/src/config.js
+++ frontend/src/config.js
@@ -1,7 +1,5 @@
 // frontend/src/config.js
-
-// Vite automatically batata hai ki hum development mein hain ya production mein
 export const BASE_URL =
   import.meta.env.MODE === "development"
-    ? "http://localhost:8000" // Jab Local chalaoge tab ye lega
-    : "https://swadkart-backend.onrender.com"; // Jab Deploy hoga tab ye lega
+    ? "http://localhost:8000"
+    : "https://swadkart-backend.onrender.com";
